089774d5016c5c14e8b38b050da6d02ef89e702a
Azharul Chowdhury <azharul.chowdhury@gmail.com>
2016-01-08 10:55:04 +0000

    fix merge conflicts in css.html
    update release number

--- dist/ngGovUk-tpls.js
+++ dist/ngGovUk-tpls.js
@@ -1,7 +1,7 @@
 /*
  * ngGovUk
  * Author: James A B Gray
- * Version: 0.0.1 - 2016-01-05
+ * Version: 0.0.1 - 2016-01-08
  * License: 
  */
 angular.module("ngGovUk", ["ngGovUk.tpls", "ngGovUk.footer","ngGovUk.global-nav","ngGovUk.nav-side","ngGovUk.tabbed-menu"]);
