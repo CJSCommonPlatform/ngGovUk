6186c3c368a6f7a6ea3442a8c649414e6df57b99
Azharul Chowdhury <azharul.chowdhury@gmail.com>
2016-02-05 12:24:56 +0000

    create new dist with new invalid alert

--- dist/ngGovUk-tpls.js
+++ dist/ngGovUk-tpls.js
@@ -1,7 +1,7 @@
 /*
  * ngGovUk
  * Author: James A B Gray
- * Version: 0.0.1 - 2016-01-12
+ * Version: 0.0.1 - 2016-02-05
  * License: 
  */
 angular.module("ngGovUk", ["ngGovUk.tpls", "ngGovUk.footer","ngGovUk.global-nav","ngGovUk.nav-side","ngGovUk.tabbed-menu"]);
