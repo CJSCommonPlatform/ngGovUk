67da6ee849270057b0263e8a06f25f2bcf3dc00d
Vygintas Ranonis <vygis@Vygintass-MBP.home>
2016-03-22 15:40:14 +0000

    added glyphicon fonts to assets and corrected their paths in the less/css files, fixed paths in dist css

--- gulp.config.js
+++ gulp.config.js
@@ -16,6 +16,8 @@
         lessPath: './dev/less',
         lessModulesPath: './dev/less/modules',
         assetPath: './dev/assets',
+        fontsPath: './dev/assets/fonts',
+        imgPath: './dev/assets/img',
         templateCachePath: './dev/templates',
         templateName: 'templates.js'
       },
@@ -24,15 +26,20 @@
         appPath: './docs/app',
         appControllerPath: './docs/app/controller',
         assetsPath: './docs/assets',
+        fontsPath: './docs/assets/fonts',
+        imgPath: './docs/assets/img',
         lessPath: './docs/assets/less',
         cssPath: './docs/assets/css',
-        cssName: 'app',
+        cssName: 'app.css',
         bowerComponentsPath: './docs/bower_components',
         bowerBootstrapLessPath: './docs/bower_components/bootstrap/less',
       },
       dist: {
         path: './dist',
         lessPath: './dist/less',
+        fontsPath: './dist/assets/fonts',
+        imgPath: './dist/assets/img',
+        cssPath: './dist/assets/css',
         lessModulesPath: './dist/less/modules',
         name: 'ngGovUk'
       }
